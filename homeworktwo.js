0eabc434d11b3dae09e76a71a3c8eda9e89938ea
RobertAghajanyan <161333612+RobertAghajanyan@users.noreply.github.com>
2024-02-26 18:19:17 +0400

    Add files via upload

---
+++ homeworktwo.js
@@ -0,0 +1,193 @@
+//classwork tasks
+
+//task1
+/* let triangle = 180;
+let numA = prompt("set first number: ");
+let numB = prompt("set second number: ");
+
+let sum = numA + numB;
+console.log("answer is: ", triangle - sum); */
+
+//task2
+
+/* let firstG = +prompt("imput salariy of first employee");
+let secondG = +prompt("imput salariy of second employee");
+let average = +prompt("imput average of 3 employees");
+
+let thirdG = average * 3 - (firstG + secondG);
+
+console.log ( "third employees salary is: ", thirdG); */
+
+//task 3
+
+/* let input = +prompt("impu your number");
+input = input % 2;
+let answer = Boolean(input);
+console.log(answer); */
+
+//task 4
+
+/* let input = +prompt("input three digit number: ");
+let num1 = input % 10;
+let num2 = Math.floor(input / 10) % 10;
+let num3 = Math.floor(input / 100) % 10;
+
+console.log ("sum is: ", num1+num2+num3); */
+
+//task 5
+
+/*let number1 = +prompt("input first number: ");
+let number2 = +prompt("input second number: ");*/
+
+//???
+
+//task 6
+
+/*let first = +prompt()
+let second = +prompt()
+let third = +prompt()
+
+let sumOfSides = first + second;
+console.log(sumOfSides > third);*/
+
+//task 7
+
+/*let num = +prompt();
+console.log(num, 2*num, 3*num);*/
+
+//task 8
+
+/*let num = +prompt();
+
+console.log(Math.round(num/50) * 50);*/
+
+//--------------------------------------------------------
+
+//homework 2 tasks
+
+//task 1
+
+/* let a = +prompt("a");
+let b = +prompt("b");
+let c = +prompt("c");
+let x = +prompt("x");
+
+console.log("Answer is: ", (a*(x*x) + (b*x) + c)) */
+
+//task 2
+
+/* let a1 = parseInt(prompt("a1"));
+let a2 = parseInt(prompt("a2"));
+let n = parseInt(prompt("n"));
+
+let d = a2 - a1;
+
+let answer = a1 + (n - 1) * d;
+
+console.log(answer); */
+
+// task 3
+
+/*let four = +prompt("insert 4 digit number: ");
+
+let firstSum = Math.floor(four / 1000) + Math.floor((four / 100) % 10);
+let lastSum = Math.floor(four % 10) + Math.floor((four % 100) / 10);
+
+console.log("Your number is: ", four, "\nfirst 2 digits sum is: ", firstSum, "\nLast 2 digits sum is: " , lastSum); */
+
+// task 4
+
+/* let number = 231;
+let strring = "4";
+
+console.log("Answer is: " , number + strring); */
+
+//task 5
+
+/* let number = 231;
+let strring = "4";
+
+console.log("Answer is: " , strring + number); */
+
+//task 6 
+
+/* let numero1 = 12;
+let numero2 = 34;
+
+console.log("num 1 is: ", numero1, "num 2 is: ", numero2, "\n", numero1 === numero2);
+
+numero1 = 55;
+numero2 = 55;
+
+console.log("num 1 is: ", numero1, "num 2 is: ", numero2, "\n", numero1 === numero2);
+
+numero1 = 1.34;
+numero2 = 1.66;
+
+console.log("num 1 is: ", numero1, "num 2 is: ", numero2, "\n", numero1 === numero2);
+
+let char1 = "5";
+numero1 = 5;
+
+console.log("num 1 is: ", numero1, "string 1 is: ", char1, "\n", numero1 === char1);
+
+console.log("num 1 is: ", numero1, "string 1 is: ", char1, "\n", numero1 == char1); */
+
+//task 7
+
+/* let txt = String(prompt("Write your text: "));
+let test = Boolean(txt);
+
+if (test == true)
+{
+    console.log("Your text is passed!");
+}
+else
+{
+    console.log("Empty text!");
+}
+*/
+
+//task 8
+
+/* let str = "88";
+let num = "99";
+
+console.log(str > num);
+console.log(str < num);
+console.log(str >= num);
+console.log(str <= num); */
+
+
+//task 9
+
+
+
+//task 10
+
+/* console.log(0/0);
+
+let nan = Number("hello world");
+
+console.log(nan); */
+
+//task 11
+
+/* let num = 10;
+let isTrue = true;
+let result = num + isTrue;
+console.log(result);
+
+var str = "Hello";
+var istrue2 = true;
+var result2 = str && isTrue;
+console.log(result); */
+
+
+
+
+
+
+
+
+
